5231655e25be813bf57664caf2acf54fc56efab9
Grant Bigelow <grantbigelow23@gmail.com>
2018-04-09 12:45:18 -0500

    js practice 1

---
+++ javascript prac 1/info.js
@@ -0,0 +1,9 @@
+var name = "Grant Bigelow";
+var d = "Retrieved on: "+new Date();
+var year = "\u00A9 2018 " + name;
+var x = "published on: 4/9/18";
+document.write("<span id ='footer'>Grant's Website</span><br />");
+document.getElementById("demo").innerHTML = d;
+document.getElementById("year").innerHTML = year;
+document.getElementById("mod").innerHTML = x;
+
